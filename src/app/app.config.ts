423ed451fba2fcf49e05e301cd5325878219e597
Nathalie Zambrano <nathalievzm@gmail.com>
2024-07-14 20:35:21 -0400

    feat (components/carousel): add styles for the movie info

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core'
 import { provideRouter, withComponentInputBinding, withViewTransitions } from '@angular/router'
+import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { provideAnimations } from '@angular/platform-browser/animations'
 import { TuiRootModule } from '@taiga-ui/core'
 
 import { routes } from './app.routes'
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { apiInterceptor } from './core/config/api.interceptor'
 
 export const appConfig: ApplicationConfig = {
